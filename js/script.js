d4c7af10525f84307cdaec0cba76f2d2f5a2cfd3
khashayar97 <khashayar979797@gmail.com>
2020-08-15 16:49:42 +0430

    edit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+var num1 = 0;
+var num2 = 0;
+var sign = null;
+
 function setvalue(num) {
     var old = document.getElementById('result').value;
     var res = 0;
@@ -7,4 +11,34 @@ function setvalue(num) {
         res = num;
     }
     document.getElementById('result').value = res;
+}
+
+function keepvalue(alamat) {
+    sign = alamat;
+    num1 = document.getElementById('result').value;
+    document.getElementById('result').value = "";
+    console.log(num1);
+}
+
+function ShowResult() {
+    num2 = document.getElementById('result').value;
+    if (sign === '+') {
+        var final = parseInt(num1) + parseInt(num2);
+    } else if (sign === '-') {
+        var final = parseInt(num1) - parseInt(num2);
+    } else if (sign === '*') {
+        var final = parseInt(num1) * parseInt(num2);
+    } else if (sign === '/') {
+        var final = parseInt(num1) / parseInt(num2);
+    }
+
+    document.getElementById('result').value = final;
+    num1 = 0;
+    num2 = 0;
+}
+
+function cls() {
+    num1 = 0;
+    num2 = 0;
+    document.getElementById('result').value = "0";
 }
